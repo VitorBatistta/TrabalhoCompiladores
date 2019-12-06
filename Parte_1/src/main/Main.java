4a78cf088021ce410d08218059db3f83cc14ea30
williamabreu <williamsantos_00@hotmail.com>
2019-12-06 00:15:28 -0300

    Add arquivo para entrega Parte 1

--- Parte_1/src/main/Main.java
+++ Parte_1/src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String...args) throws IOException {
         // Arquivo de entrada
-        String filePath = "src/test/Teste_01.jmm";
+        String filePath = "Parte_1/src/test/correto.jmm";
         ANTLRFileStream input = new ANTLRFileStream(filePath);
 
         // Parser, Scanner
